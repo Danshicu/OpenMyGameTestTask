7ba44418959def41cc4ca33d256c753027253767
Danshicu <Promchuk.danik@gmail.com>
2023-09-17 22:39:01 +0300

    Added ChessScene

--- Assets/App/Scripts/Scenes/SceneChess/Features/GridNavigation/Navigator/ChessGridNavigator.cs
+++ Assets/App/Scripts/Scenes/SceneChess/Features/GridNavigation/Navigator/ChessGridNavigator.cs
@@ -8,20 +8,120 @@ namespace App.Scripts.Scenes.SceneChess.Features.GridNavigation.Navigator
 {
     public class ChessGridNavigator : IChessGridNavigator
     {
-        /*
-         Pawn - пешка
-         Bishop - слон (по диагонали)
-         Knight - конь
-         Rook - Ладья (по сторонам)
-         Queen - королева
-         King - король
-         */
         public List<Vector2Int> FindPath(ChessUnitType unit, Vector2Int from, Vector2Int to, ChessGrid grid)
         {
-            //напиши реализацию не меняя сигнатуру функции
-            //ChessUnitType type = ChessUnitType.King;
-            //type.GetPossibleMoves();
-            throw new NotImplementedException();
+            Queue<PathNode> nodesQueue = new Queue<PathNode>();
+            List<PathNode> visitedNodes = new List<PathNode>();
+            Vector2Int gridSize = grid.Size;
+            
+            List<List<PathNode>> nodes = CreateNodesList(gridSize);
+
+            PathNode startNode = nodes[from.x][from.y];
+            PathNode endNode = nodes[to.x][to.y];
+            PathNode currentNode;
+            
+            nodesQueue.Enqueue(startNode);
+            visitedNodes.Add(startNode);
+
+            while (nodesQueue.Count > 0)
+            {
+                currentNode = nodesQueue.Dequeue();
+                if (currentNode == endNode)
+                {
+                    return GetPath(startNode, currentNode);
+                }
+
+                List<PathNode> neighbourNodes = new List<PathNode>();
+                AddNeighboursNodes(currentNode, ref neighbourNodes, unit, ref grid, ref nodes, ref visitedNodes);
+                foreach (var neigbhour in neighbourNodes)
+                {
+                    if (visitedNodes.Contains(neigbhour))
+                    {
+                        continue;
+                    }
+                    visitedNodes.Add(neigbhour);
+                    neigbhour.ParentNode = currentNode;
+                    nodesQueue.Enqueue(neigbhour);
+                }
+                
+            }
+
+            return null;
+        }
+
+        private List<List<PathNode>> CreateNodesList(Vector2Int size)
+        {
+            List<List<PathNode>> nodes = new List<List<PathNode>>();
+            for (int firstIndex = 0; firstIndex < size.y; firstIndex++)
+            {
+                nodes.Add(new List<PathNode>());
+                for (int secondIndex = 0; secondIndex < size.x; secondIndex++)
+                {
+                    nodes[firstIndex].Add(new PathNode(firstIndex, secondIndex));
+                }
+            }
+
+            return nodes;
+        }
+
+        private List<Vector2Int> GetPath(PathNode startNode, PathNode endNode)
+        {
+            PathNode currentNode = endNode;
+            List<Vector2Int> path = new List<Vector2Int>();
+            path.Add(currentNode.Position);
+
+            while (currentNode.ParentNode != startNode)
+            {
+                currentNode = currentNode.ParentNode;
+                path.Add(currentNode.Position);
+            }
+            
+            path.Reverse();
+
+            return path;
+        }
+
+        private static bool CanMove(ref ChessGrid grid, Vector2Int position)
+        {
+            Vector2Int gridSize = grid.Size;
+            return position.x < gridSize.x && position.x >= 0 && position.y < gridSize.y && position.y >= 0 &&
+                   grid.Get(position.y, position.x) == null;
+        }
+
+        private void AddNeighboursNodes(PathNode currentNode, ref List<PathNode> neighbors, ChessUnitType unit, ref ChessGrid grid, ref List<List<PathNode>> nodes, ref List<PathNode> visitedNodes)
+        {
+            var moves = unit.GetPossibleMoves();
+            int maxDistance = 1;
+            if (moves.Item2)
+            {
+                if (grid.Size.x > grid.Size.y)
+                {
+                    maxDistance = grid.Size.x;
+                }
+                else
+                {
+                    maxDistance = grid.Size.y;
+                }
+            }
+            
+            foreach (var direction in moves.Item1)
+            {
+                for (int moveRound = 1; moveRound <= maxDistance; moveRound++)
+                {
+                    Vector2Int newPosition = currentNode.Position + (direction * moveRound);
+                    if (CanMove(ref grid, newPosition) &&
+                        !visitedNodes.Contains(nodes[newPosition.x][newPosition.y]))
+                    {
+                        neighbors.Add(nodes[newPosition.x][newPosition.y]);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            
+            
         }
     }
 }
---
+++ Assets/App/Scripts/Scenes/SceneChess/Features/GridNavigation/Navigator/PathNode.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace App.Scripts.Scenes.SceneChess.Features.GridNavigation.Navigator
+{
+    public class PathNode
+    {
+        public Vector2Int Position { get; }
+        public PathNode ParentNode;
+
+        public PathNode(int posX, int posY)
+        {
+            Position = new Vector2Int(posX, posY);
+        }
+
+    }
+}
